db0158de95fcbf948b384d8582c80cf1377f5e16
Edison <princehan920453024@gmail.com>
2023-08-30 17:10:04 -0400

    update action generation from keyboard input based on updated mapping.

--- ml-agents-envs/mlagents_envs/key2action.py
+++ ml-agents-envs/mlagents_envs/key2action.py
@@ -23,26 +23,26 @@ class Key2Action:
         self.last_key = key.char
 
     def get_multi_discrete_action(self):
-        action = [0] * 4
+        action = [1] * 4
         if self.last_key is None:
             return action
 
         if self.last_key == 'w':
-            action[0] = 1
+            action[0] = 0
         elif self.last_key == 's':
-            action[0] = -1
+            action[0] = 2
         elif self.last_key == 'a':
-            action[1] = 1
+            action[1] = 0
         elif self.last_key == 'd':
-            action[1] = -1
+            action[1] = 2
         elif self.last_key == 'i':
-            action[2] = 1
+            action[2] = 0
         elif self.last_key == 'k':
-            action[2] = -1
+            action[2] = 2
         elif self.last_key == 'j':
-            action[3] = 1
+            action[3] = 0
         elif self.last_key == 'l':
-            action[3] = -1
+            action[3] = 2
         else:
             print(f'Unrecognized key {self.last_key}')
 
@@ -77,14 +77,14 @@ class Key2Action:
         return [action]
 
     def get_random_action(self, multi_discrete: bool = False):
-        if multi_discrete:
-            action = [0] * 4
+        if multi_discrete:  # one-hot
+            action = [1] * 4
             axis = random.randint(0, 3)
-            direction = random.randint(1, 2)
+            direction = random.randint(0, 2)
             action[axis] = direction
             return action
         else:
-            return [random.randint(0, 9)]
+            return [random.randint(0, 8)]
 
 
 if __name__ == '__main__':
