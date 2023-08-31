0625acb1bf12aa43031aeb5d6d4c57aa0718db60
Edison <princehan920453024@gmail.com>
2023-08-31 14:20:59 -0400

    update image collection code.

--- ml-agents-envs/mlagents_envs/envs/collect_images.py
+++ ml-agents-envs/mlagents_envs/envs/collect_images.py
@@ -8,26 +8,25 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    env_path = '/home/edison/Terrain/circular_river_4d/circular_river_action4d.x86_64'
+    env_path = '/home/edison/Terrain/circular_river_collision/circular_river_collision.x86_64'
     img_dir_name = 'sim_images'
     img_save_dir = '/home/edison/Research/ml-agents/ml-agents-envs/mlagents_envs/envs/' + img_dir_name
     os.makedirs(img_save_dir, exist_ok=True)
     print(f'Saving images to {img_save_dir} ...')
 
     max_img_num = 2000
-    wait_frame_num = 5
+    wait_frame_num = 3
 
     unity_env = UnityEnvironment(file_name=env_path, no_graphics=False, seed=1,
                                  additional_args=['-logFile', 'unity.log'])
-    env = UnityToGymWrapper(unity_env, uint8_visual=True, flatten_branched=False, encode_obs=False, wait_frames_num=0)
+    env = UnityToGymWrapper(unity_env, uint8_visual=True, flatten_branched=False,
+                            encode_obs=False, wait_frames_num=wait_frame_num)
     print(f'Env loaded!')
 
     i = -1
     while (i := i + 1) < max_img_num:
-        obs = env.reset()
-        j = -1
-        while (j := j + 1) < wait_frame_num:
-            obs, _, _, _ = env.step([1] * 4)
+        env.reset()
+        obs, _, _, _ = env.step([1] * 4)  # no action to make view stable
         # print(f'{obs.shape=}')
         obs_path = os.path.join(img_save_dir, ('%04d' % i) + '.jpg')
         plt.imsave(obs_path, obs)
