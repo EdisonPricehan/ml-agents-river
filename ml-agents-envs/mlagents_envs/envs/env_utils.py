78fe5c21165e5ada1226fbdef591009f4c1cb745
Edison <princehan920453024@gmail.com>
2025-03-07 20:43:30 -0500

    comment out vec_env make function for stable_baseline3.

--- ml-agents-envs/mlagents_envs/envs/env_utils.py
+++ ml-agents-envs/mlagents_envs/envs/env_utils.py
@@ -2,9 +2,9 @@ from mlagents_envs.environment import UnityEnvironment
 
 from mlagents_envs.envs.unity_gym_env import UnityToGymWrapper
 
-from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
-from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
-from stable_baselines3.common.monitor import Monitor
+# from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
+# from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
+# from stable_baselines3.common.monitor import Monitor
 
 from mlagents_envs.side_channel.environment_parameters_channel import EnvironmentParametersChannel
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel, EngineConfig
@@ -50,51 +50,51 @@ def make_unity_env(
     return env
 
 
-def make_unity_vec_env(env_directory: str,
-                       num_env: int,
-                       visual: bool,
-                       seed: int,
-                       start_index: int = 0,
-                       flatten_discrete_action: bool = False,
-                       safer_l: bool = False,
-                       no_graphics: bool = False,
-):
-    """
-    Create a wrapped, monitored vector Unity environment.
-    """
-
-    def make_env(rank, seed, use_visual=True):  # pylint: disable=C0111
-        def _thunk():
-            width, height = 128, 128
-            channel_env = EnvironmentParametersChannel()
-            channel_env.set_float_parameter("simulation_mode", 1.0)
-
-            channel_eng = EngineConfigurationChannel()
-            channel_eng.set_configuration_parameters(width=width, height=height, quality_level=1, time_scale=1,
-                                                     target_frame_rate=50, capture_frame_rate=50)
-
-            channel_agent = AgentConfigurationChannel()
-            channel_agent.set_configuration_parameters(max_idle_steps=0)  # set to 0 to disable idle reset
-
-            channel_reset = AgentResetChannel()
-            channel_reset.set_reset_pose(random_reset=True)
-
-            unity_env = UnityEnvironment(env_directory, base_port=5000 + rank, seed=seed, no_graphics=no_graphics,
-                                         side_channels=[channel_env, channel_eng, channel_agent, channel_reset])
-
-            env = UnityToGymWrapper(unity_env, uint8_visual=use_visual, flatten_branched=flatten_discrete_action,
-                                    allow_multiple_obs=True, action_space_seed=seed, safe_rl=safer_l)
-
-            # new_logger = configure("/tmp/unity_sb3_ppo_log/", ["stdout", "csv", "tensorboard"])
-            # env = Monitor(env, filename=new_logger.get_dir())
-            # env = Monitor(env)
-            # env = RolloutInfoWrapper(env)
-            return env
-
-        return _thunk
-
-    if visual:
-        return SubprocVecEnv([make_env(i + start_index, seed) for i in range(num_env)])
-    else:
-        rank = MPI.COMM_WORLD.Get_rank() if MPI else 0
-        return DummyVecEnv([make_env(rank, use_visual=False, seed=seed)])
+# def make_unity_vec_env(env_directory: str,
+#                        num_env: int,
+#                        visual: bool,
+#                        seed: int,
+#                        start_index: int = 0,
+#                        flatten_discrete_action: bool = False,
+#                        safer_l: bool = False,
+#                        no_graphics: bool = False,
+# ):
+#     """
+#     Create a wrapped, monitored vector Unity environment.
+#     """
+#
+#     def make_env(rank, seed, use_visual=True):  # pylint: disable=C0111
+#         def _thunk():
+#             width, height = 128, 128
+#             channel_env = EnvironmentParametersChannel()
+#             channel_env.set_float_parameter("simulation_mode", 1.0)
+#
+#             channel_eng = EngineConfigurationChannel()
+#             channel_eng.set_configuration_parameters(width=width, height=height, quality_level=1, time_scale=1,
+#                                                      target_frame_rate=50, capture_frame_rate=50)
+#
+#             channel_agent = AgentConfigurationChannel()
+#             channel_agent.set_configuration_parameters(max_idle_steps=0)  # set to 0 to disable idle reset
+#
+#             channel_reset = AgentResetChannel()
+#             channel_reset.set_reset_pose(random_reset=True)
+#
+#             unity_env = UnityEnvironment(env_directory, base_port=5000 + rank, seed=seed, no_graphics=no_graphics,
+#                                          side_channels=[channel_env, channel_eng, channel_agent, channel_reset])
+#
+#             env = UnityToGymWrapper(unity_env, uint8_visual=use_visual, flatten_branched=flatten_discrete_action,
+#                                     allow_multiple_obs=True, action_space_seed=seed, safe_rl=safer_l)
+#
+#             # new_logger = configure("/tmp/unity_sb3_ppo_log/", ["stdout", "csv", "tensorboard"])
+#             # env = Monitor(env, filename=new_logger.get_dir())
+#             # env = Monitor(env)
+#             # env = RolloutInfoWrapper(env)
+#             return env
+#
+#         return _thunk
+#
+#     if visual:
+#         return SubprocVecEnv([make_env(i + start_index, seed) for i in range(num_env)])
+#     else:
+#         rank = MPI.COMM_WORLD.Get_rank() if MPI else 0
+#         return DummyVecEnv([make_env(rank, use_visual=False, seed=seed)])
